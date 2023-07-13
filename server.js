c68ca417f2bb056e7b52b83c50239272c537bc90
Michael Guedko <mjgeko@gmail.com>
2023-07-13 13:50:58 -0400

    Configure cors and express

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+//Routers
+
+
+const app = express();
+// Express 4.0
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+//CORS
+app.use(cors({
+    origin: '*', // Change to origin: ['https://example.com', 'https://another.example.com']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    limit: '10mb',
+}))
