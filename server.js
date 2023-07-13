1494db8494dabe2505c5f15d0e5a905efef265b8
Michael Guedko <mjgeko@gmail.com>
2023-07-13 13:53:41 -0400

    Configure middleware

--- server.js
+++ server.js
@@ -12,13 +12,23 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-// Express 4.0
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
 //CORS
 app.use(cors({
     origin: '*', // Change to origin: ['https://example.com', 'https://another.example.com']
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     limit: '10mb',
-}))
+}));
+//Use react/vite for views
+app.use(express.static(path.join(__dirname, "client", "dist")));
+//Middleware
+app.use(logger("dev"));
+app.use(express.json({})); //Use json instead of html
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
+
+//Connect to db
+
+//Routes
